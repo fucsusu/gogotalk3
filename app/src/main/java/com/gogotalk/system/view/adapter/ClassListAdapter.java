eb6fb80deafb2cc4c978d4570dd5fbf3b41a7dec
wuhongjie <wuhongjie-1234@163.com>
2019-08-07 14:45:37 +0800

    优化预约弹框UI

--- app/src/main/java/com/gogotalk/system/view/adapter/ClassListAdapter.java
+++ app/src/main/java/com/gogotalk/system/view/adapter/ClassListAdapter.java
@@ -129,6 +129,7 @@ public class ClassListAdapter extends RecyclerView.Adapter<ClassListAdapter.View
             AppUtils.bindImageToView(context
                     , url, R.mipmap.ic_main_list_item_header_default
                     , holder.mImgs, DiskCacheStrategy.ALL,true,9);
+            holder.stye = 0;
             if (holder.stye == 0) {
                 holder.mLayout3.setVisibility(View.GONE);//锁的背景Layout
                 holder.mBtn.setBackgroundResource(R.mipmap.bg_main_list_item_btn_go_room);//立即预约按钮颜色
